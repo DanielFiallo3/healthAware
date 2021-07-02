64e28fba8bb5b97c746407893aed070ac01e0a77
Ananya Hans <ananyahanss@gmail.com>
2021-07-02 16:26:36 -0500

    added content to prof page

--- starter/react-app/src/components/User.js
+++ starter/react-app/src/components/User.js
@@ -10,9 +10,53 @@ function User() {
 
   return (
     <div>
-      <h1>Welcome {user.name} {user.allergies.map(ale => ( 
-        <h3>{ale.name}</h3>
-       ))}</h1>
+      <h1 >Welcome {user.name} </h1>
+      
+      <div>
+        {user.profilePic}
+      </div>
+
+      <div>
+        {user.vaccinationCard}
+      </div>
+      
+      <div>
+        {user.geolocation}
+      </div>
+
+      <div> 
+        I am allergic to:
+          {user.allergies.map(ale => ( 
+            <li key="allergy">{ale.name}</li>
+          ))}
+      </div>
+
+      <div>
+        Severity:
+          {user.severity}
+      </div>
+
+      <div>
+        Curent Symptoms:
+          <li> {user.currentSymptoms} </li>
+      </div>
+
+      <div>
+        Additional Details:
+          <li> {user.additionalDetails} </li>
+      </div>
+
+      <div>
+        Email:
+          <li> {user.email} </li>
+      </div>
+
+      <div>
+        Username:
+          <li> {user.username} </li>
+      </div>
+
+
     </div>
   );
 }
